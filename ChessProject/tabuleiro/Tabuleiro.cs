4ca2a2b7b741d5a47cdf37655a88877c62ef2dd4
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-06-24 10:26:41 -0300

    Inicio da implementação do Tabuleiro.

---
+++ ChessProject/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessProject.tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int linhas {  get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+    }
+}
