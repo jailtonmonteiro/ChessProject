eb738fb9033fe9585fb276876324eb1bdefe64af
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-06-24 17:46:03 -0300

    Melhorando exibição de tabuleiro.

--- ChessProject/Tela.cs
+++ ChessProject/Tela.cs
@@ -9,6 +9,7 @@ namespace ChessProject
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for(int j = 0; j < tab.colunas; j++)
                 {
                     if(tab.peca(i, j) == null)
@@ -22,6 +23,7 @@ namespace ChessProject
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
         }
     }
 }
---
+++ ChessProject/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿using tabuleiro;
+using System;
+
+namespace xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
