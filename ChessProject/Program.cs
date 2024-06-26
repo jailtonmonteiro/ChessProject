b93748f39fc7048a6390371801ae8100e6882b73
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-06-26 14:37:31 -0300

    Inserindo cores e peças pretas com o uso da função ConsoleColor

--- ChessProject/Program.cs
+++ ChessProject/Program.cs
@@ -15,6 +15,8 @@ namespace ChessProject
                 tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 6));
+                tab.colocarPeca(new Rainha(Cor.Branca, tab), new Posicao(4, 7));
+                tab.colocarPeca(new Peao(Cor.Branca, tab), new Posicao(5, 5));
 
                 Tela.imprimirTabuleiro(tab);
             }
--- ChessProject/Tela.cs
+++ ChessProject/Tela.cs
@@ -18,12 +18,29 @@ namespace ChessProject
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else 
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+
         }
     }
 }
