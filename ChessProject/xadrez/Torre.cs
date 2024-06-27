586b1816d127bc3b8333ac0c46a673ecfa1dcdcb
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-06-27 17:27:49 -0300

    Implementação de movimentos da Torre.

--- ChessProject/xadrez/Torre.cs
+++ ChessProject/xadrez/Torre.cs
@@ -13,5 +13,63 @@ namespace xadrez
         {
             return "T";
         }
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != this.cor;
+        }
+
+        public override bool[,] movimentoPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //Norte
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            while(tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha - 1;
+            }
+            //Leste
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            while(tab.peca(pos) != null && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if(tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+            }
+            //Sul
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            while(tab.peca(pos) != null && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if(tab.peca(pos) != null && podeMover(pos))
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 1;
+            }
+            //Oeste
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            while (tab.peca(pos) != null && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+            }
+            return mat;
+        }
     }
 }
